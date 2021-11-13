0c750ddf686e5c74ef683a9bb51d1295671eaf38
diego caraballo <diego.caraballo@cavepot.com>
2021-11-13 17:52:10 -0300

    Se agrega funcionalidad que da de alta un gasto

--- src/interfaz/scripts/index.js
+++ src/interfaz/scripts/index.js
@@ -4,10 +4,15 @@ import { MDCTabBar } from '@material/tab-bar';
 import { MDCTextField } from '@material/textfield';
 import { MDCSelect } from '@material/select';
 import {MDCSnackbar} from '@material/snackbar';
+import Handler from '../../dominio/objects/handler';
+import Transaction from '../../dominio/objects/transaction';
+import User from '../../dominio/objects/user';
+
 // import ListaPeliculas from '../../dominio/lista-peliculas.mjs';
 // import Pelicula from '../../dominio/pelicula.mjs';
 
 // const listaPeliculas = new ListaPeliculas();
+const handler = new Handler();
 
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
@@ -29,20 +34,25 @@ const textFieldExpenseAmount = new MDCTextField(document.getElementById('expense
 const textFieldExpenseDate = new MDCTextField(document.getElementById('expenseDate'));
 //TODO: agregar los inputs faltantes
 
-// const addButton = new MDCRipple(document.getElementById('addButton'));
-// addButton.listen('click', () => {
-//   // let title = textFieldTitle.value;
-//   // let year = textFieldYear.value;
-//   // let genre = selectGenre.value;
-//   try {
-//     // let newPelicula = new Pelicula(title, genre, year);
-//     // listaPeliculas.agregar(newPelicula);
-//   } catch (error) {
-//     // const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-//     // snackbar.labelText = error.message;
-//     // snackbar.open();
-//   } finally {
-//     // let peliculas = listaPeliculas.getPeliculas();
-//     // console.log(peliculas);
-//   }
-// })
+const addExpenseButton = new MDCRipple(document.getElementById('addExpenseButton'));
+
+addExpenseButton.listen('click', () => {
+  //TODO: hacer funcion para el add de expense
+  let expenseName = textFieldExpenseName.value;
+  let expenseCategory = textFieldExpenseCategory.value;
+  let expenseAmount = textFieldExpenseAmount.value;
+  let expenseDate = textFieldExpenseDate.value;
+  const newUser = new User(1, "test", 20, "email", "password", 100);
+
+  try {
+    let newTransaction = new Transaction(1, newUser, expenseName, expenseCategory, expenseAmount, expenseDate);
+    handler.addTransaction(newTransaction);
+  } catch (error) {
+    const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
+    snackbar.labelText = error.message;
+    snackbar.open();
+  } finally {
+    let transactions = handler.getTransactionsByUser(newUser);
+    console.log(transactions);
+  }
+})
