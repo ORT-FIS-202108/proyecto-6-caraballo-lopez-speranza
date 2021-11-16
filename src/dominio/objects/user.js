a788b017dc8654681fc7c00b8afa24979042004f
diego caraballo <diego.caraballo@cavepot.com>
2021-11-16 19:11:35 -0300

    se agrega formulario para dar de alta un ingreso

--- src/dominio/objects/user.js
+++ src/dominio/objects/user.js
@@ -1,12 +1,13 @@
 export default class User {
+  static USER_ID = 1;
 
   constructor(id, name, age, email, password) {
-
     this.id = id;
     this.name = name;
     this.age = age;
     this.email = email;
     this.password = password;
     this.balance = 0;
+    this.USER_ID++;
   }
 }
