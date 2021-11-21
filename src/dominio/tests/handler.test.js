5945614c0033fe83d1e6c9e57685cd150947405d
Francisco Lopez <Flopezvaccaro@gmail.com>
2021-11-21 15:10:31 -0300

    se llega al 100 de cobertura de user y transaction y se trabaja en la cobertura de handler

--- src/dominio/tests/handler.test.js
+++ src/dominio/tests/handler.test.js
@@ -1,32 +1,87 @@
 import Handler from '../objects/handler.mjs';
 import User from '../objects/user.mjs';
+import Transaction from '../objects/transaction.mjs';
+
+let handler;
+
+
+beforeEach(() => {
+  handler = new Handler();
+});
 
 test('create handler variable users', () => {
-  const handler = new Handler();
   expect(handler.users).toEqual([]);
 });
 
 test('create handler variable transactions', () => {
-  const handler = new Handler();
   expect(handler.transactions).toEqual([]);
 });
 
 test('create handler variable activeUser', () => {
-  const handler = new Handler();
   expect(handler.activeUser).toEqual(undefined);
 });
 
-test('setActiveUser', () => {
-  const handler = new Handler();
-  const testUser = new User('Fran', 16, 'unMail@mail.com', 'sads32sasdwasdwe');
-  handler.setActiveUser(testUser);
-  expect(handler.activeUser).toEqual(testUser);
+describe('Active User tests', () => {
+  let testUser;
+  beforeEach(() => {
+    testUser = new User('Fran', 16, 'unMail@mail.com', 'sads32sasdwasdwe');
+  });
+
+  test('setActiveUser', () => {
+    handler.setActiveUser(testUser);
+    expect(handler.activeUser).toEqual(testUser);
+  });
+
+  test('getActiveUser', () => {
+    handler.setActiveUser(testUser);
+    expect(handler.getActiveUser()).toEqual(testUser);
+  });
 });
 
-test('getActiveUser', () => {
-  const handler = new Handler();
-  const testUser = new User('Fran', 16, 'unMail@mail.com', 'sads32sasdwasdwe');
-  handler.setActiveUser(testUser);
-  expect(handler.getActiveUser()).toEqual(testUser);
+describe('ABM User tests', () => {
+  let testUser;
+  beforeEach(() => {
+    testUser = new User('Fran', 16, 'unMail@mail.com', 'sads32sasdwasdwe');
+  });
+  test('addUser', () => {
+    handler.addUser(testUser);
+    expect(handler.users[0]).toEqual(testUser);
+  });
+  test('getUsers', () => {
+    handler.addUser(testUser);
+    expect(handler.users).toEqual(handler.getUsers());
+  });
+  test('editUser', () => {
+    handler.addUser(testUser);
+    testUser.name = 'francisco';
+    handler.editUser(testUser);
+    expect(handler.users[0].name).toEqual('francisco');
+  });
+  test('existsUser', () => {
+    handler.addUser(testUser);
+    expect(handler.existsUser('unMail@mail.com')).toEqual(true);
+  });
+  test('not existsUser', () => {
+    handler.addUser(testUser);
+    expect(handler.existsUser('unMasdasail@mail.com')).toEqual(false);
+  });
 });
 
+describe('ABM User tests', () => {
+  let testUser;
+  let testTransaction;
+  beforeEach(() => {
+    testUser = new User('Fran', 16, 'unMail@mail.com', 'sads32sasdwasdwe');
+    handler.addUser(testUser);
+    handler.setActiveUser(testUser);
+    testTransaction = new Transaction(testUser, 'transaccion prueba', 'unaCategoria', 1000, '10/11/2021', 'expense');
+  });
+  test('add transaction', () => {
+    handler.addTransaction(testTransaction);
+    expect(handler.transactions[0]).toEqual(testTransaction);
+  });
+  test('add transaction', () => {
+    handler.addTransaction(testTransaction);
+    expect(handler.getTransactionsByUser()).toEqual([testTransaction]);
+  });
+});
---
+++ src/dominio/tests/user.test.js
@@ -0,0 +1,49 @@
+import User from '../objects/user.mjs';
+
+
+describe('contructor User tests', () => {
+  const testUser1 = new User('Francisco', 34, 'unmail@mail.com', 'contraseña123');
+  test('constructor name', () => {
+    expect(testUser1.name).toEqual('Francisco');
+  });
+  test('constructor age', () => {
+    expect(testUser1.age).toEqual(34);
+  });
+  test('constructor email', () => {
+    expect(testUser1.email).toEqual('unmail@mail.com');
+  });
+  test('constructor password', () => {
+    expect(testUser1.password).toEqual('contraseña123');
+  });
+  test('constructor id', () => {
+    expect(testUser1.id).toEqual(1);
+  });
+  test('constructor balance', () => {
+    expect(testUser1.balance).toEqual(0);
+  });
+  test('id updater', () => {
+    expect(User.USER_ID).toEqual(2);
+  });
+});
+
+describe('validateUser tests', () => {
+  const correctName = 'Francisco';
+  const correctAge = '38';
+  const correctEmail= 'unmail@mail.com';
+  const correctPassword = 'contraseña123';
+  test('validateUser incorrectName', () => {
+    expect(User.validateUser('', correctAge, correctEmail, correctPassword)).toEqual('El nombre ingresado no es valido');
+  });
+  test('validateUser incorrectAge', () => {
+    expect(User.validateUser(correctName, -34, correctEmail, correctPassword)).toEqual('La edad ingresada no es valida');
+  });
+  test('validateUser incorrectEmail', () => {
+    expect(User.validateUser(correctName, correctAge, 'no soy un email', correctPassword)).toEqual('El correo ingresado no es valido');
+  });
+  test('validateUser incorrectEmail', () => {
+    expect(User.validateUser(correctName, correctAge, correctEmail, '123')).toEqual('La contraseña ingresada no es valida');
+  });
+  test('validateUser incorrectEmail', () => {
+    expect(User.validateUser(correctName, correctAge, correctEmail, correctPassword)).toEqual(undefined);
+  });
+});
